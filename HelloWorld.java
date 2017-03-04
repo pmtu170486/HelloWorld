e0258c31e3022f32dcf56ded394a09961a0aa602
pmtu170486 <pmtu@fit.hcmus.edu.vn>
2017-03-04 11:11:24 +0700

    Modify by Join

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld
 
 	public static void main (String[] args)
 	{
-		System.out.println(message + " - " + name);
+		System.out.println(message + " / " + name); // Modify by Join
 	}
 
 }
